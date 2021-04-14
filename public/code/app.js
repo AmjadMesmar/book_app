e4b4b985b9913cbd622f4fe269af3511b0edba26
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-04-15 00:08:14 +0300

    done with lab 13

---
+++ public/code/app.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+'use strict';
+console.log('js working');
+
+
+$('#updateSection').hide();
+$('#updateButton').on('click',function(){
+  $('#updateSection').toggle();
+});
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable quotes */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
@@ -11,16 +12,18 @@ const express = require('express');
 const pg = require('pg'); // install pg package:  npm i pg
 const cors = require('cors'); //CORS = Cross Origin Resource Sharing
 const superagent = require('superagent'); // client-side HTTP request library.
+const methodOverride = require('method-override');
+
 
 const PORT = process.env.PORT || 3000;
 
 
 // adding DATABASE URL for localhost:
-// const client = new pg.Client(process.env.DATABASE_URL);
 
-// clients.connect(); // Activate the client   //better use the connect function at the end of the code.
+const client = new pg.Client(process.env.DATABASE_URL);
 
 // adding DATABASE URL for Heroku:
+
 // const client = new pg.Client({ connectionString: process.env.DATABASE_URL,
 //      ssl: { rejectUnauthorized: false } });
 
@@ -30,11 +33,17 @@ app.set('view engine','ejs');
 app.use(express.static('./public'));
 app.use(cors());
 app.use( express.urlencoded( {extended:true} ) ); // for POST method to work.
+app.use (methodOverride('_method'));
 
 // Routes:
 
 app.get('/', homeRouteHandler);//home route
+app.get('/search_book', searchPageHandler);
+app.post('/add_book', addBookHandler);
+app.post('/details/:bookID', showBookDetails);
+app.put('/updateBook/:bookID',updateBookHandler);
 app.post('/search', searchHandler);//hello route
+app.delete('/deleteBook/:bookID',deleteBookHandler);
 app.get('*', notFoundHandler); //error handler
 
 
@@ -42,7 +51,19 @@ app.get('*', notFoundHandler); //error handler
 // Routes handlers:
 
 function homeRouteHandler(req, res) {
-  res.render('pages/index');
+  let SQL = `SELECT * FROM books;`;
+  client.query(SQL)
+    .then (result=>{
+      res.render('pages/index',{booksArr:result.rows});
+      // res.send(result.rows);
+    })
+    .catch(error=>{
+      res.send(error);
+    });
+}
+
+function searchPageHandler (req,res){
+  res.render('pages/searches/search-books');
 }
 
 function searchHandler (req,res){
@@ -64,19 +85,84 @@ function searchHandler (req,res){
     });
 }
 
+function addBookHandler (req,res) {
+  let bookImage = req.body.image;
+  let bookTitle = req.body.title;
+  let bookAuthor = req.body.author;
+  let bookIsbn = req.body.isbn;
+  let bookDate = req.body.publishDate;
+  let bookDescription = req.body.description;
+
+  let SQL = `SELECT * FROM books WHERE title=$1`;
+  let bookCheck = [bookTitle];
+  client.query(SQL, bookCheck).then(result => {
+    // console.log(result);
+    if (result.rowCount) {
+      // res.send(result.rows[0]);
+      console.log('Book already added');
+      res.send('book already exist');
+    }
+    else {
+      SQL = `INSERT INTO books (image_url,title,author,isbn,date,description) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;`;
+      let safeValues = [bookImage,bookTitle,bookAuthor,bookIsbn,bookDate,bookDescription];
+      client.query(SQL,safeValues)
+        .then(result=>{
+          // res.send(result);
+          console.log('New Book added');
+          // res.send('new book added');
+          res.render('pages/details',{book:result.rows[0]});
+
+        });
+    }
+  });
+}
+
+function showBookDetails(req,res) {
+  let SQL = `SELECT * FROM books WHERE id=$1;`;
+  let value = [req.params.bookID];
+  // console.log('value',value);
+  client.query(SQL,value)
+    .then((result) => {
+      // console.log('bookid',result);
+      res.render('pages/details',{book:result.rows[0]});
+    });
+}
+
+function updateBookHandler (req,res){
+  console.log(req.params.bookID);
+  let {image,title,author,isbn,publishDate,description} = req.body;
+  let SQL = `UPDATE books SET image_url=$1,title=$2,author=$3,isbn=$4,date=$5,description=$6 WHERE id=$7;`;
+  let safeValues = [image,title,author,isbn,publishDate,description,req.params.bookID];
+  client.query(SQL,safeValues)
+    .then(()=>{
+      res.redirect(`/`);
+    }).catch(error=>{
+      res.send(error);
+    });
+
+}
+
+function deleteBookHandler(req,res) {
+  let SQL = `DELETE FROM books WHERE id=$1;`;
+  let value = [req.params.bookID];
+  client.query(SQL,value)
+    .then(res.redirect('/'));
+}
+
 
 function notFoundHandler(req, res) {
   res.render('pages/error');
 }
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`listening on port ${PORT}`);
+// });
 
 function Book (bookData){
 
   this.title = (bookData.volumeInfo.title) ? bookData.volumeInfo.title : 'Not avialable';
   this.author = (bookData.volumeInfo.authors) ? bookData.volumeInfo.authors.join(',') : 'Not avialable';
+  this.isbn = (bookData.volumeInfo.industryIdentifiers[0].identifier) ? bookData.volumeInfo.industryIdentifiers[0].identifier : 'Not Available';
   this.date = (bookData.volumeInfo.publishedDate) ? bookData.volumeInfo.publishedDate : 'Not avialable';
   this.cover = (bookData.volumeInfo.imageLinks) ? bookData.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
   this.description = (bookData.volumeInfo.description) ? bookData.volumeInfo.description : 'Not avialable';
@@ -84,10 +170,10 @@ function Book (bookData){
 }
 
 
-// client.connect()
-//   .then(() => {
-//     app.listen(PORT, () =>
-//       console.log(`listening on ${PORT}`)
-//     );
-//   });
+client.connect()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`listening on ${PORT}`)
+    );
+  });
 
